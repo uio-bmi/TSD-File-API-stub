133dc73e96cc261696391ae831c2b053cbd318dc
dtitov <dmytro.titov@gmail.com>
2019-12-04 17:20:19 +0100

    Add Docker Compose file

--- src/main/java/no/uio/ifi/tsd/config/SwaggerConfig.java
+++ src/main/java/no/uio/ifi/tsd/config/SwaggerConfig.java
@@ -6,7 +6,6 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,8 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
 
         return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors
-                        .basePackage("no.uio.ifi.tsd"))
+                .apis(RequestHandlerSelectors.basePackage("no.uio.ifi.tsd"))
                 .paths(PathSelectors.any())
                 .build().apiInfo(apiEndPointsInfo());
     }
